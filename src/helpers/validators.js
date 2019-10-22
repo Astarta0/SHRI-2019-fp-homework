c9e4ba649e4a283463c0bbf31e5a28ed8a9a2ef8
Anastasia Zaytseva <anastasia.zaytseva0@yandex.ru>
2019-10-22 23:00:38 +0300

    fix task1

--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -23,48 +23,48 @@ import {replace, length, compose, test, allPass, anyPass, complement} from 'ramd
 
 const replaceNotNumbers = replace(/[^0-9]/g, '');
 const replaceNumbers = replace(/[0-9]/g, '');
-const getNumbersCount = compose(length, replaceNotNumbers);
-
-const getLengthValidatePredicate = fn => compose(fn, length);
-const getNumbersCountValidatePredicate = fn => compose(fn, getNumbersCount);
+const isLengthLess = count => length => length < count;
+const isLengthMore = count => length => length > count;
 
 /**
  * Функции для проверки выполнения условий с количеством цифр в строке
  */
-const isNumbersCountMore = count => length => length > count;
-const isNumbersCountLess = count => length => length < count;
+
+const getNumbersCount = compose(length, replaceNotNumbers);
+const getNumbersCountValidatePredicate = fn => compose(fn, getNumbersCount);
 
 /**
  * Функции для проверки выполнения условий с длиной строки
  */
 
-const isLengthLess = count => length => length < count;
-const isLengthMore = count => length => length > count;
+const getLengthValidatePredicate = fn => compose(fn, length);
 
 /**
  * Функции для проверки наличия конкретного символа в строке
  */
 
-const isContains = (regexp, replaceFn) => compose(test(regexp), replaceFn);
-const isNotContains = (regexp, replaceFn) => complement(isContains(regexp, replaceFn));
+const isNotContainsStrict = (regexp) => complement(test(regexp));
+
+const isContainsNoStrict = (regexp, replaceFn) => compose(test(regexp), replaceFn);
+const isNotContainsNoStrict = (regexp, replaceFn) => complement(isContainsNoStrict(regexp, replaceFn));
 
 // 1. Длина < 5 и кол-во цифр > 2 шт.
 export const validateFieldN1 = (value) => {
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountMore(2));
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthMore(2));
     const isLengthValid = getLengthValidatePredicate(isLengthLess(5));
     return allPass([isLengthValid, isNumCountValid])(value);
 };
 
 // 2. Длина < 5 и кол-во цифр < 2 шт.
 export const validateFieldN2 = (value) => {
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountLess(2));
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthLess(2));
     const isLengthValid = getLengthValidatePredicate(isLengthLess(5));
     return allPass([isLengthValid, isNumCountValid])(value);
 };
 
 // 3. Длина > 5 или кол-во цифр > 1 шт.
 export const validateFieldN3 = (value) => {
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountMore(1));
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthMore(1));
     const isLengthValid = getLengthValidatePredicate(isLengthMore(5));
     return anyPass([isLengthValid, isNumCountValid])(value);
 };
@@ -72,38 +72,38 @@ export const validateFieldN3 = (value) => {
 // 4. Длина < 10 и кол-во цифр > 2 шт. и одна из цифр равна "4"
 export const validateFieldN4 = (value) => {
     const isLengthValid = getLengthValidatePredicate(isLengthLess(10));
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountMore(2));
-    return allPass([isLengthValid, isNumCountValid, isContains(/4/, replaceNotNumbers)])(value);
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthMore(2));
+    return allPass([isLengthValid, isNumCountValid, isContainsNoStrict(/4/, replaceNotNumbers)])(value);
 };
 
 // 5. Длина < 10 и кол-во цифр > 1 шт. и ни одна из цифр не равна "4"
 export const validateFieldN5 = (value) => {
     const isLengthValid = getLengthValidatePredicate(isLengthLess(10));
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountMore(1));
-    return allPass([isLengthValid, isNumCountValid, isNotContains(/4/g, replaceNotNumbers)])(value);
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthMore(1));
+    return allPass([isLengthValid, isNumCountValid, isNotContainsNoStrict(/4/g, replaceNotNumbers)])(value);
 };
 
 // 6. Длина > 5, или одна из цифр равна "7"
 export const validateFieldN6 = (value) => {
     const isLengthValid = getLengthValidatePredicate(isLengthMore(5));
-    const contains7 = isContains(/7/, replaceNotNumbers);
+    const contains7 = isContainsNoStrict(/7/, replaceNotNumbers);
     return anyPass([isLengthValid, contains7])(value);
 };
 
 // 7. Длина > 8 и кол-во цифр > 3 шт. и только англ
 export const validateFieldN7 = (value) => {
     const isLengthValid = getLengthValidatePredicate(isLengthMore(8));
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountMore(3));
-    const onlyEng = isNotContains(/[^A-Za-z]/g, replaceNumbers);
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthMore(3));
+    const onlyEng = isNotContainsNoStrict(/[^A-Za-z]/g, replaceNumbers);
 
     return allPass([isLengthValid, isNumCountValid, onlyEng])(value);
 };
 
 // 8. Кол-во цифр < 5 шт. или только англ или одна из цифр равна "7"
 export const validateFieldN8 = (value) => {
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountLess(5));
-    const onlyEng = isNotContains(/[^A-Za-z0-9]/g, replaceNumbers);
-    const contains7 = isContains(/7/, replaceNotNumbers);
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthLess(5));
+    const onlyEng = isNotContainsStrict(/[^A-Za-z]/g);
+    const contains7 = isContainsNoStrict(/7/, replaceNotNumbers);
 
     return anyPass([isNumCountValid, onlyEng, contains7])(value);
 };
@@ -111,8 +111,8 @@ export const validateFieldN8 = (value) => {
 // 9. Длина < 8, кол-во цифр > 4 шт. только англ
 export const validateFieldN9 = (value) => {
     const isLengthValid = getLengthValidatePredicate(isLengthLess(8));
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountMore(4));
-    const onlyEng = isNotContains(/[^A-Za-z]/g, replaceNumbers);
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthMore(4));
+    const onlyEng = isNotContainsNoStrict(/[^A-Za-z]/g, replaceNumbers);
 
     return allPass([isLengthValid, isNumCountValid, onlyEng])(value);
 };
@@ -120,8 +120,8 @@ export const validateFieldN9 = (value) => {
 // 10. Длина < 4 или кол-во цифр > 2 шт. или только англ
 export const validateFieldN10 = (value) => {
     const isLengthValid = getLengthValidatePredicate(isLengthLess(4));
-    const isNumCountValid = getNumbersCountValidatePredicate(isNumbersCountMore(2));
-    const onlyEng = isNotContains(/[^A-Za-z0-9]/g, replaceNumbers);
+    const isNumCountValid = getNumbersCountValidatePredicate(isLengthMore(2));
+    const onlyEng = isNotContainsStrict(/[^A-Za-z]/g);
 
     return anyPass([isNumCountValid, onlyEng, isLengthValid])(value);
 };
